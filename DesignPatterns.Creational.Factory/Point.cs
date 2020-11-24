6a7a7f447d5636c240e901ca5f6ef664f7bc67f8
Saba Gotsiridze <gotsiridze.s@outlook.com>
2020-11-24 22:32:22 +0400

    Add Factory Method

---
+++ DesignPatterns.Creational.Factory/Point.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Creational.Factory
+{
+    public enum CoordinateSystem
+    {
+        Cartesian,
+        Polar
+    }
+
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point() { }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        #region Factory Method
+        
+        //private Point(double x, double y)
+        //{
+        //    X = x;
+        //    Y = y;
+        //}
+
+        //// Factory Method for point class
+        //public static Point NewCartesianPoint(double x, double y)
+        //{
+        //    return new Point(x, y);
+        //}
+
+        //public static Point NewPolarPoint(double rhoe, double theta)
+        //{
+        //    return new Point(rhoe * Math.Cos(theta), rhoe * Math.Sin(theta));
+        //}
+        
+        #endregion
+    }
+}
